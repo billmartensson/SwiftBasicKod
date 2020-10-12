89112761af564862f891c8b201a84d6519d90cc0
Bill Mårtensson <bill@magictechnology.se>
2020-10-12 09:59:50 +0200

    Start

--- SwiftBasicKod/ViewController.swift
+++ SwiftBasicKod/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
+    
+    var myNumber = 7
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("viewDidLoad")
+        
+        print(myNumber)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("viewDidAppear")
+        
+        var myNumber = 123
+        
+        myNumber = 99
+        
+        print(myNumber)
+        
+        let calcResult = letsCalculate(thenumber: 23, otherNumber: 3)
+        
+        print(calcResult)
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        print("viewWillAppear")
+    }
 
+    func letsCalculate(thenumber : Int, otherNumber : Int) -> String
+    {
+        let timesTwoNumber = thenumber * otherNumber
+        
+        let resultText = "\(thenumber) gånger \(otherNumber) är \(timesTwoNumber)"
+        
+        return resultText
+    }
+    
 }
 
